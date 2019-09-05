f5e2ff4abe5b5411de2eee38f8d0d4fca08bbefd
Keurfon Luu <keurfonluu@outlook.com>
2019-09-05 16:53:14 -0700

    add function to write tough input file

---
+++ toughio/model/io/__init__.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+
+"""
+Author: Keurfon Luu <keurfonluu@lbl.gov>
+License: MIT
+"""
+
+from .common import (
+    Parameters,
+    new,
+)
+from .write import write
+
+__all__ = [
+    "Parameters",
+    "new",
+    "write",
+]
