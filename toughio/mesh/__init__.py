b5e4ed91358b857f0ede952a25584877929675ee
Keurfon Luu <keurfonluu@outlook.com>
2020-01-30 15:51:15 -0800

    update toughio.mesh init

--- toughio/mesh/__init__.py
+++ toughio/mesh/__init__.py
@@ -8,23 +8,20 @@ License: MIT
 import meshio
 
 from . import utils
-from . import spatial
 from .helpers import (
     read,
     write,
     write_points_cells,
 )
-from ._mesh import Mesh
+from ._mesh import Mesh, from_meshio
 from ._common import get_meshio_version
 
 if get_meshio_version() < (3,3,0):
-    from meshio import (
-        XdmfTimeSeriesReader,
-        XdmfTimeSeriesWriter,
-    )
+    from meshio import XdmfTimeSeriesReader as TimeSeriesReader
+    from meshio import XdmfTimeSeriesWriter as TimeSeriesWriter
 else:
-    from meshio.xdmf import TimeSeriesReader as XdmfTimeSeriesReader
-    from meshio.xdmf import TimeSeriesWriter as XdmfTimeSeriesWriter
+    from meshio.xdmf import TimeSeriesReader
+    from meshio.xdmf import TimeSeriesWriter
 
 
 __all__ = [
@@ -32,8 +29,8 @@ __all__ = [
     "write",
     "write_points_cells",
     "Mesh",
-    "XdmfTimeSeriesReader",
-    "XdmfTimeSeriesWriter",
+    "TimeSeriesReader",
+    "TimeSeriesWriter",
+    "from_meshio",
     "utils",
-    "spatial",
 ]
