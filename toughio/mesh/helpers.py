7c07189053a0fdb838fc71ef31fbe702f4fb143c
Keurfon Luu <keurfonluu@outlook.com>
2020-01-29 22:44:25 -0800

    remove eclipse reader

--- toughio/mesh/helpers.py
+++ toughio/mesh/helpers.py
@@ -8,7 +8,6 @@ License: MIT
 from ._mesh import Mesh, _meshio_to_toughio_mesh
 from . import tough
 from . import flac3d_io
-from . import eclipse_io
 import meshio
 
 __all__ = [
@@ -20,7 +19,6 @@ __all__ = [
 
 _extension_to_filetype = {
     ".f3grid": "flac3d",
-    ".grdecl": "eclipse",
 }
 
 
@@ -48,52 +46,20 @@ def read(filename, file_format = None, **kwargs):
         Input file format. If `None`, it will be guessed from file's
         extension.
 
-    Other Parameters
-    ----------------
-    repair_conformity : bool, optional, default False
-        Only if ``file_format == 'eclipse'``. Repair mesh conformity
-        (reconnect adjacent cells) by splitting bricks as smaller bricks.
-    repair_cells : bool, optional, default False
-        Only if ``file_format == 'eclipse'``. Convert bricks as hexahedra,
-        wedges, pyramids or tetrahedra depending on the number of unique
-        points. Repaired mesh may be semi-conformal (i.e. at least one
-        cell completely shares its face with its neighbor).
-    z_precision : int or None, optional, default None
-        Only if ``file_format == 'eclipse'``. If not `None`, round corner
-        depths to z_precision decimal points (only if ``repair_conformity == True``).
-    invert_zaxis: bool, optional, default False
-        Only if ``file_format == 'eclipse'``. If `True`, invert Z axis by
-        multiplying Z coordinates by -1.
-
     Returns
     -------
-    Mesh
+    toughio.Mesh
         Imported mesh.
-
-    Note
-    ----
-    This function wraps function ``meshio.read`` by adding support to
-    Eclipse GRDECL and FLAC3D grids. Therefore, all the formats compatible
-    with ``meshio`` are also supported.
     """
     # Check file format
     assert isinstance(filename, str)
     fmt = file_format if file_format else _filetype_from_filename(filename)
 
-    # Call custom readers for TOUGH, FLAC3D and Eclipse
+    # Call custom readers for TOUGH and FLAC3D
     format_to_reader = {
         "tough": ( tough, (), {} ),
         "flac3d": ( flac3d_io, (), {} ),
         "flac3d-ascii": ( flac3d_io, (), {} ),
-        "eclipse": (
-            eclipse_io, (),
-            {
-                "repair_conformity": False,
-                "repair_cells": False,
-                "z_precision": None,
-                "invert_zaxis": False,
-            },
-        ),
     }
     if fmt in format_to_reader.keys():
         interface, args, default_kwargs = format_to_reader[fmt]
@@ -113,29 +79,22 @@ def write(filename, mesh, file_format = None, **kwargs):
     ----------
     filename : str
         Output file name.
-    mesh : Mesh
+    mesh : toughio.Mesh
         Mesh to export.
     file_format : str or None, optional, default None
         Output file format. If `None`, it will be guessed from file's
         extension. To write TOUGH MESH, `file_format` must be specified
         as 'tough' (no specific extension exists for TOUGH MESH).
-
-    Note
-    ----
-    This function wraps functions ``meshio.write`` by adding support to
-    TOUGH and FLAC3D grids. Therefore, all the formats compatible with
-    ``meshio`` are also supported.
     """
     # Check file format
     assert isinstance(filename, str)
     fmt = file_format if file_format else _filetype_from_filename(filename)
 
-    # Call custom writer for TOUGH, FLAC3D and Eclipse
+    # Call custom writer for TOUGH and FLAC3D
     format_to_writer = {
         "tough": ( tough, (), {} ),
         "flac3d": ( flac3d_io, (), {} ),
         "flac3d-ascii": ( flac3d_io, (), {} ),
-        "eclipse": ( eclipse_io, (), {} ),
     }
     if fmt in format_to_writer.keys():
         interface, args, default_kwargs = format_to_writer[fmt]
