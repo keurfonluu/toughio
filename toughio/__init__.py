8ce122c60f22ed6e2f6d1bcf3cf97aab30148bcc
Keurfon Luu <keurfonluu@outlook.com>
2020-02-02 22:13:46 -0800

    add function/method read_output

--- toughio/__init__.py
+++ toughio/__init__.py
@@ -15,11 +15,13 @@ from .__about__ import (
 from . import mesh
 from . import model
 from .mesh import Mesh
+from ._helpers import read_output
 
 __all__ = [
     "Mesh",
     "mesh",
     "model",
+    "read_output",
     "__version__",
     "__author__",
     "__author_email__",
