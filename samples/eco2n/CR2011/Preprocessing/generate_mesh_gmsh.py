280801202a70ff999374396ca5551f63edebcfb1
Keurfon Luu <keurfonluu@outlook.com>
2020-02-08 15:07:39 -0800

    update sample scripts

--- samples/eco2n/CR2011/Preprocessing/generate_mesh_gmsh.py
+++ samples/eco2n/CR2011/Preprocessing/generate_mesh_gmsh.py
@@ -8,40 +8,33 @@ import toughio
 mesh = toughio.mesh.read("gmsh/mesh.msh")
 
 # Process grid
-mesh.points[:,[1,2]] = mesh.points[:,[2,1]]
+mesh.points[:, [1, 2]] = mesh.points[:, [2, 1]]
 mesh.extrude_to_3d(
-    height = 1.,
+    height = 1.0,
     axis = 1,
 )
 
 # Define boundary conditions
-bc = numpy.zeros(mesh.n_cells)
 materials = numpy.concatenate(mesh.materials)
-idx = numpy.nonzero(materials == "BOUND")[0]
-bc[idx] = 1
-mesh.cell_data["boundary_condition"] = mesh.split(bc)
+bcond = (materials == "BOUND").astype(int)
+mesh.cell_data["boundary_condition"] = mesh.split(bcond)
 
 # Define initial conditions
-incon = numpy.full((mesh.n_cells, 4), -1.0e9)
 centers = numpy.concatenate(mesh.centers)
-incon[:,0] = 1.0e5 - 9810.0 * centers[:,2]
-incon[:,1] = 0.05
-incon[:,2] = 0.0
-incon[:,3] = 10.0 - 0.025 * centers[:,2]
+incon = numpy.full((mesh.n_cells, 4), -1.0e9)
+incon[:, 0] = 1.0e5 - 9810.0 * centers[:, 2]
+incon[:, 1] = 0.05
+incon[:, 2] = 0.0
+incon[:, 3] = 10.0 - 0.025 * centers[:, 2]
 mesh.cell_data["initial_condition"] = mesh.split(incon)
 
-# Define permeability
+# Define permeability modifiers
 permeability = numpy.full((mesh.n_cells, 3), -1.0)
-idx = numpy.nonzero(materials == "CENAQ")[0]
-permeability[idx] = [1.0e-13, 1.0e-13, 1.0e-15]
+permeability[materials == "CENAQ"] = [1.0e-13, 1.0e-13, 1.0e-15]
 mesh.cell_data["permeability"] = mesh.split(permeability)
 
 # Export MESH and INCON
-mesh.to_tough(
-    "MESH",
-    nodal_distance = "orthogonal",
-    incon_eos = "eco2n",
-)
+mesh.to_tough("MESH", incon_eos = "eco2n")
 
 # Pickle for faster later import
 mesh.write("mesh.pickle")
