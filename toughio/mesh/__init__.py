ecf7756e95192737e91d47cdd19b28d184c7794e
Keurfon Luu <keurfonluu@outlook.com>
2020-02-10 00:12:30 -0800

    temporarily remove support for time series

--- toughio/mesh/__init__.py
+++ toughio/mesh/__init__.py
@@ -4,20 +4,10 @@ from ._common import get_meshio_version
 from ._helpers import read, write, write_points_cells
 from ._mesh import Mesh, from_meshio
 
-if get_meshio_version() < (3, 3, 0):
-    from meshio import XdmfTimeSeriesReader as TimeSeriesReader
-    from meshio import XdmfTimeSeriesWriter as TimeSeriesWriter
-else:
-    from meshio.xdmf import TimeSeriesReader
-    from meshio.xdmf import TimeSeriesWriter
-
-
 __all__ = [
     "read",
     "write",
     "write_points_cells",
     "Mesh",
-    "TimeSeriesReader",
-    "TimeSeriesWriter",
     "from_meshio",
 ]
