5eee03ffc95fc55ca2397ab3f8f55dcacc9e0037
Keurfon Luu <keurfonluu@outlook.com>
2020-02-11 00:13:26 -0800

    attempt fixing pypi for python 2.7

--- setup.py
+++ setup.py
@@ -10,15 +10,18 @@ with open(os.path.join(base_dir, "toughio", "__about__.py"), "rb") as f:
 
 
 DISTNAME = "toughio"
-DESCRIPTION = "toughio"
-LONG_DESCRIPTION = "toughio"
+DESCRIPTION = "Pre- and post-processing Python library for TOUGH"
+LONG_DESCRIPTION = open("README.md").read()
 VERSION = about["__version__"]
 AUTHOR = about["__author__"]
 AUTHOR_EMAIL = about["__author_email__"]
 URL = about["__website__"]
 LICENSE = about["__license__"]
 CLASSIFIERS = [
-    "Programming Language :: Python",
+    "Programming Language :: Python :: 2.7",
+    "Programming Language :: Python :: 3.5",
+    "Programming Language :: Python :: 3.6",
+    "Programming Language :: Python :: 3.7",
     "Development Status :: 5 - Production/Stable",
     "License :: OSI Approved :: MIT License",
     "Natural Language :: English",
@@ -44,7 +47,7 @@ if sys.version_info < (3,):
     REQUIREMENTS = ["meshio == 2.3.10"]
     EXTRA_REQUIREMENTS = {"full": ["pyvista == 0.22.4"]}
 else:
-    REQUIREMENTS = ["meshio >= 3.0.0"]
+    REQUIREMENTS = ["meshio >= 3.3.1"]
     EXTRA_REQUIREMENTS = {"full": ["pyvista >= 0.23.1"]}
 REQUIREMENTS += ["numpy >= 1.13.0"]
 EXTRA_REQUIREMENTS["full"] += ["scipy>=0.9"]
@@ -65,6 +68,7 @@ if __name__ == "__main__":
         license=LICENSE,
         install_requires=REQUIREMENTS,
         extras_require=EXTRA_REQUIREMENTS,
+        python_requires=">=2.7,!=3.0.*,!=3.1.*,!=3.2.*,!=3.3.*,!=3.4.*",
         classifiers=CLASSIFIERS,
         version=about["__version__"],
         packages=find_packages(),
