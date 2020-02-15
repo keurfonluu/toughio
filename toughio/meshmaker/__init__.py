49880dc8c82e99d93dc70e919e72cb38ba0fd61e
Keurfon Luu <keurfonluu@outlook.com>
2020-02-14 18:13:33 -0800

    add meshmaker voxelize

--- toughio/meshmaker/__init__.py
+++ toughio/meshmaker/__init__.py
@@ -1,7 +1,9 @@
 from ._structured_grid import structured_grid
 from ._triangulate import triangulate
+from ._voxelize import voxelize
 
 __all__ = [
+    "voxelize",
     "structured_grid",
     "triangulate",
 ]
---
+++ toughio/meshmaker/_voxelize.py
@@ -0,0 +1,41 @@
+import numpy
+
+from ._structured_grid import structured_grid
+
+__all__ = [
+    "voxelize",
+]
+
+
+def voxelize(points, material="dfalt"):
+    """
+    Generate a 1D hexahedral mesh from 1D point coordinates.
+
+    Parameters
+    ----------
+    points : array_like (n_points,)
+        Cooordinates of points.
+    material : str, optional, default 'dfalt'
+        Default material name.
+
+    Returns
+    -------
+    toughio.Mesh
+        Output hexahedral mesh.
+    """
+    assert isinstance(points, (list, tuple, numpy.ndarray))
+    assert numpy.ndim(points) == 1
+
+    points = numpy.asarray(points)
+    idx = numpy.argsort(points)
+    dx = 0.5 * (points[idx[1:]] - points[idx[:-1]])
+    dx = numpy.concatenate(([dx[0]], dx))
+    dy = [0.05 * (points.max() - points.min())]
+    dz = dy
+    origin = -0.5 * numpy.array([dx[0], dy[0], dz[0]])
+
+    mesh = structured_grid(dx, dy, dz, origin)
+    cell_type, cells = mesh.cells[0]
+    mesh.cells = [(cell_type, cells[idx])]
+
+    return mesh
