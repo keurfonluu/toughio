d2950846e47b3d74b45e33988fe86c926162a427
Keurfon Luu <keurfonluu@outlook.com>
2020-02-25 00:47:37 -0800

    add sample mesh

--- test/helpers.py
+++ test/helpers.py
@@ -2,6 +2,35 @@ import os
 
 import numpy
 
+import toughio
+
+
+hybrid_mesh = toughio.Mesh(
+    points=numpy.array([
+        [0.0, 0.0, 0.0],
+        [1.0, 0.0, 0.0],
+        [1.0, 1.0, 0.0],
+        [0.0, 1.0, 0.0],
+        [0.0, 0.0, 1.0],
+        [1.0, 0.0, 1.0],
+        [1.0, 1.0, 1.0],
+        [0.0, 1.0, 1.0],
+        [0.5, 0.5, 1.5],
+        [0.0, 0.5, 1.5],
+        [1.0, 0.5, 1.5],
+        [2.0, 0.0, 0.0],
+        [2.0, 1.0, 0.0],
+        [-1.0, 0.0, 0.0],
+        [-1.0, 1.0, 0.0],
+    ]),
+    cells=[
+        ("hexahedron", numpy.array([[0, 1, 2, 3, 4, 5, 6, 7]])),
+        ("pyramid", numpy.array([[4, 5, 6, 7, 8]])),
+        ("tetra", numpy.array([[4, 8, 7, 9], [5, 6, 8, 10]])),
+        ("wedge", numpy.array([[1, 11, 5, 2, 12, 6], [13, 0, 4, 14, 3, 7]])),
+    ],
+)
+
 
 def relative_permeability(model, parameters, sl=None, atol=1.0e-8):
     import json
