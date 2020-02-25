62f3146cf04d5af227a82c8826f00a49987bd62d
Keurfon Luu <keurfonluu@outlook.com>
2020-02-25 00:44:03 -0800

    add function read_time_series

--- toughio/mesh/__init__.py
+++ toughio/mesh/__init__.py
@@ -1,12 +1,13 @@
 import meshio
 
-from ._helpers import read, write, write_points_cells, write_time_series
+from ._helpers import read, write, write_points_cells, read_time_series, write_time_series
 from ._mesh import Mesh, from_meshio
 
 __all__ = [
     "read",
     "write",
     "write_points_cells",
+    "read_time_series",
     "write_time_series",
     "Mesh",
     "from_meshio",
