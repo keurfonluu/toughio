c26e3b305e7100129618823b2e353f65b92c5b22
Keurfon Luu <keurfonluu@outlook.com>
2020-03-19 18:48:14 -0700

    update clean.sh

--- notebooks/clean.sh
+++ notebooks/clean.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-rm -f `ls | grep -v "^clean.sh$\|^run.sh$\|^CO2TAB$\|^Preprocessing$\|^Postprocessing$"`
+rm -f `ls | grep -v "^clean.sh$\|^run.sh$\|^CO2TAB$\|^Preprocessing$\|^Postprocessing$\|^README.md$"`
 rm -rf TABLE .OUTPUT_*
