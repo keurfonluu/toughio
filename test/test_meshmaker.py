f053839cca1f86dca9c7431945eec209778fba0b
Keurfon Luu <keurfonluu@outlook.com>
2020-04-04 20:07:17 -0700

    add test_triangulate

--- test/test_meshmaker.py
+++ test/test_meshmaker.py
@@ -1,6 +1,15 @@
 import numpy
+import pytest
 
 import toughio
+
+
+@pytest.mark.parametrize("ndim", [2, 3])
+def test_triangulate(ndim):
+    points = numpy.random.rand(100, ndim)
+    mesh = toughio.meshmaker.triangulate(points)
+    assert numpy.allclose(points, mesh.points)
+    assert mesh.cells[0].type == ("triangle" if ndim == 2 else "tetra")
 
 
 def test_voxelize():
