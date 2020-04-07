791f240fec9b86cde5a67b1c3c5ad1cbacb02653
Keurfon Luu <keurfonluu@outlook.com>
2020-04-06 21:20:58 -0700

    fix json

--- toughio/_io/json/_json.py
+++ toughio/_io/json/_json.py
@@ -2,6 +2,8 @@ from __future__ import with_statement
 
 import json
 
+import numpy
+
 __all__ = [
     "read",
     "write",
@@ -31,10 +33,16 @@ def read(filename):
     with open(filename, "r") as f:
         parameters = json.load(f)
 
-    keys = {"extra_options", "more_options", "selections"}
-    for key in keys:
-        if key in parameters.keys():
-            parameters[key] = to_int(parameters[key])
+    if "extra_options" in parameters.keys():
+        parameters["extra_options"] = to_int(parameters["extra_options"])
+
+    if "more_options" in parameters.keys():
+        parameters["more_options"] = to_int(parameters["more_options"])
+
+    if "selections" in parameters.keys():
+        parameters["selections"]["integers"] = to_int(
+            parameters["selections"]["integers"]
+        )
 
     return parameters
 
@@ -51,5 +59,22 @@ def write(filename, parameters):
         Parameters to export.
 
     """
+    from copy import deepcopy
+
+    def jsonify(x):
+        """JSON serialize data."""
+        if isinstance(x, (numpy.int32, numpy.int64)):
+            return int(x)
+        elif isinstance(x, (list, tuple)):
+            return [jsonify(xx) for xx in x]
+        elif isinstance(x, numpy.ndarray):
+            return x.tolist()
+        elif isinstance(x, dict):
+            return {k: jsonify(v) for k, v in x.items()}
+        else:
+            return x
+
     with open(filename, "w") as f:
+        parameters = deepcopy(parameters)
+        parameters = jsonify(parameters)
         json.dump(parameters, f, indent=4)
