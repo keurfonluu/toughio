3b1e527049c2030f727947e84c534e2266e7f243
Keurfon Luu <keurfonluu@outlook.com>
2020-05-28 21:02:49 -0700

    handle connection data in standard tough output

--- toughio/_io/output/tough/_tough.py
+++ toughio/_io/output/tough/_tough.py
@@ -13,17 +13,22 @@ __all__ = [
 def read(filename, file_type, file_format, labels_order):
     """Read standard TOUGH OUTPUT."""
     with open(filename, "r") as f:
-        headers, times, variables = _read_table(f)
+        headers, times, variables = _read_table(f, file_type)
 
-        headers = headers[2:]
-        labels = [[v[0] for v in variable] for variable in variables]
-        variables = numpy.array([[v[2:] for v in variable] for variable in variables])
+        ilab = 1 if file_type == "element" else 2
+        headers = headers[ilab + 1:]
+        labels = [[v[:ilab] for v in variable] for variable in variables]
+        labels = [[l[0] for l in label] for label in labels] if file_type == "element" else labels
+        variables = numpy.array([[v[ilab + 1:] for v in variable] for variable in variables])
 
     return to_output(file_type, file_format, labels_order, headers, times, labels, variables)
 
 
-def _read_table(f):
+def _read_table(f, file_type):
     """Read data table for current time step."""
+    labels_key = "ELEM." if file_type == "element" else "ELEM1"
+    ilab = 1 if file_type == "element" else 2
+
     times, variables = [], []
     while True:
         line = f.readline().strip()
@@ -35,10 +40,10 @@ def _read_table(f):
             times.append(float(line.split()[0]))
             variables.append([])
 
-            # Look for "ELEM."
+            # Look for "ELEM." or "ELEM1"
             while True:
                 line = f.readline().strip()
-                if line.startswith("ELEM."):
+                if line.startswith(labels_key):
                     break
 
             # Read headers
@@ -53,9 +58,9 @@ def _read_table(f):
             # Loop until end of output block
             while True:
                 if line[:10].strip() and not line.strip().startswith("ELEM"):
-                    line = line.strip()
-                    tmp = [line[:5]]
-                    tmp += [str2float(x) for x in line[5:].split()]
+                    line = line.strip().split()
+                    tmp = [l.strip() for l in line[:ilab]]
+                    tmp += [str2float(l) for l in line[ilab:]]
                     variables[-1].append(tmp)
 
                 line = f.readline()
