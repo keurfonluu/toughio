c36f7a57cdc3667473a2797c951bd9b7ade1f5c6
Keurfon Luu <keurfonluu@outlook.com>
2020-05-30 00:50:17 -0700

    add extra dependencies for dev

--- setup.py
+++ setup.py
@@ -51,6 +51,9 @@ else:
     EXTRA_REQUIREMENTS = {"full": ["pyvista >= 0.23.1"]}
 REQUIREMENTS += ["numpy >= 1.13.0"]
 EXTRA_REQUIREMENTS["full"] += ["matplotlib", "netcdf4", "h5py", "scipy>=0.9"]
+EXTRA_REQUIREMENTS["dev"] = list(EXTRA_REQUIREMENTS["full"])
+EXTRA_REQUIREMENTS["dev"] += ["black", "isort", "docformatter"]
+
 
 PACKAGE_DATA = {
     "toughio.data": ["CO2TAB"],
