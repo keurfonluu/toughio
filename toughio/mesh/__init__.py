31e49f105f3253db625faff63a9e03267f941183
Keurfon Luu <keurfonluu@outlook.com>
2020-05-30 18:55:42 -0700

    remove function write_points_cells

--- toughio/mesh/__init__.py
+++ toughio/mesh/__init__.py
@@ -5,7 +5,6 @@ from ._helpers import (
     read,
     read_time_series,
     write,
-    write_points_cells,
     write_time_series,
 )
 from ._mesh import Mesh, from_meshio, from_pyvista
@@ -13,7 +12,6 @@ from ._mesh import Mesh, from_meshio, from_pyvista
 __all__ = [
     "read",
     "write",
-    "write_points_cells",
     "read_time_series",
     "write_time_series",
     "Mesh",
