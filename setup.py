8032b2771c13e914d19dcf4b34cbbca6302d6cc7
Keurfon Luu <keurfonluu@outlook.com>
2020-08-05 21:49:44 -0700

    support python 3.8

--- setup.py
+++ setup.py
@@ -22,6 +22,7 @@ CLASSIFIERS = [
     "Programming Language :: Python :: 3.5",
     "Programming Language :: Python :: 3.6",
     "Programming Language :: Python :: 3.7",
+    "Programming Language :: Python :: 3.8",
     "Development Status :: 5 - Production/Stable",
     "License :: OSI Approved :: BSD License",
     "Natural Language :: English",
