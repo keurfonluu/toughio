7e0b24cdbecae8bbaaa006c336f2d5b49a0c52af
Keurfon Luu <keurfonluu@outlook.com>
2020-09-04 18:41:34 -0700

    fix filter box

--- toughio/_mesh/_filter/box/_box.py
+++ toughio/_mesh/_filter/box/_box.py
@@ -9,9 +9,9 @@ __all__ = [
 
 def filter_(mesh, x0=None, y0=None, z0=None, dx=None, dy=None, dz=None):
     """Box filter."""
-    xmin = x0 if x0 else -numpy.inf
-    ymin = y0 if y0 else -numpy.inf
-    zmin = z0 if z0 else -numpy.inf
+    xmin = x0 if x0 is not None else -numpy.inf
+    ymin = y0 if y0 is not None else -numpy.inf
+    zmin = z0 if z0 is not None else -numpy.inf
 
     xmax = xmin + dx if dx else numpy.inf
     ymax = ymin + dy if dy else numpy.inf
