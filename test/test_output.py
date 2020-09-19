4542afdb647ec2860e9f3c023e86c2ad777a47f0
Keurfon Luu <keurfonluu@outlook.com>
2020-09-18 19:46:50 -0700

    fix tough3 history reader

--- test/test_output.py
+++ test/test_output.py
@@ -22,6 +22,16 @@ write_read = lambda output, writer_kws, reader_kws: helpers.write_read(
         (
             "FOFT_A1912.csv",
             {
+                "TIME": 4.393722000e9,
+                "PRES": 1.8740899675005e8,
+                "TEMP": 720.0,
+                "SAT_G": 0.0,
+                "SAT_L": 24.0,
+            },
+        ),
+        (
+            "FOFT_A1912_T2.csv",
+            {
                 "TIME": 3.06639400e9,
                 "PRES": 1.83000721e8,
                 "TEMP": 660.0,
@@ -31,6 +41,10 @@ write_read = lambda output, writer_kws, reader_kws: helpers.write_read(
         ),
         (
             "GOFT_A1162.csv",
+            {"TIME": 4.393722000e9, "GEN": -30.0, "ENTG": 1.528048035348e7, "PWB": 0.0},
+        ),
+        (
+            "GOFT_A1162_T2.csv",
             {"TIME": 3.06639400e9, "GEN": -27.5, "ENTG": 1.40141971e7, "PWB": 0.0},
         ),
     ],
