a079966cbdc614f1dee04be6b6bc3a599024b32f
Keurfon Luu <keurfonluu@outlook.com>
2020-10-06 21:40:43 -0700

    add function register_output

--- toughio/_io/output/__init__.py
+++ toughio/_io/output/__init__.py
@@ -1,9 +1,10 @@
 from . import csv, petrasim, save, tecplot, tough
 from ._common import Output
-from ._helpers import read, read_history, write
+from ._helpers import register, read, read_history, write
 
 __all__ = [
     "Output",
+    "register",
     "read",
     "write",
     "read_history",
