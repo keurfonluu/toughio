858abadf2cc038fd9812c537ecb3d055a2370743
Keurfon Luu <keurfonluu@outlook.com>
2020-10-06 21:38:05 -0700

    add function register_mesh

--- toughio/_mesh/__init__.py
+++ toughio/_mesh/__init__.py
@@ -1,10 +1,11 @@
 import meshio
 
 from . import avsucd, flac3d, pickle, tecplot, tough
-from ._helpers import read, read_time_series, write, write_time_series
+from ._helpers import register, read, read_time_series, write, write_time_series
 from ._mesh import CellBlock, Mesh, from_meshio, from_pyvista
 
 __all__ = [
+    "register",
     "read",
     "write",
     "read_time_series",
