a64dbbee427cb695b78fe6194f26e230b866155f
Keurfon Luu <keurfonluu@outlook.com>
2020-11-03 17:57:56 -0800

    fix scalar

--- examples/co2_leakage_along_a_fault/1_generate_mesh_in_python_with_gmsh.py
+++ examples/co2_leakage_along_a_fault/1_generate_mesh_in_python_with_gmsh.py
@@ -313,6 +313,7 @@ pyvista.set_plot_theme("document")
 p = pyvista.Plotter(window_size=(800, 800))
 p.add_mesh(
     mesh=pyvista.read("mesh.msh"),
+    scalars="gmsh:physical",
     stitle="Materials",
     show_scalar_bar=True,
     show_edges=True,
