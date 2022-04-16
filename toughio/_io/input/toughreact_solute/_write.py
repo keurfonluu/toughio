f28e13ae22f931b52b3cdc5e90c49f862ca40ce3
Keurfon Luu <keurfonluu@outlook.com>
2022-04-16 00:54:23 -0700

    support writing solute input file

--- toughio/_io/input/toughreact_solute/_write.py
+++ toughio/_io/input/toughreact_solute/_write.py
@@ -1,7 +1,5 @@
 import logging
 
-import numpy as np
-
 from .._common import to_str
 from ...._common import open_file
 
@@ -10,14 +8,14 @@ __all__ = [
 ]
 
 
-def write(filename, parameters, mopr_11=0, verbose=True):
-    buffer = write_buffer(parameters, mopr_11, verbose)
+def write(filename, parameters, mopr_10=0, mopr_11=0, verbose=True):
+    buffer = write_buffer(parameters, mopr_10, mopr_11, verbose)
     with open_file(filename, "w") as f:
         for record in buffer:
             f.write(record)
 
 
-def write_buffer(parameters, mopr_11=0, verbose=True):
+def write_buffer(parameters, mopr_10=0, mopr_11=0, verbose=True):
     numbers = _generate_numbers(parameters)
 
     # Define input file contents
@@ -28,26 +26,16 @@ def write_buffer(parameters, mopr_11=0, verbose=True):
     out += _write_weights_coefficients(parameters, verbose)
     out += _write_convergence(parameters, verbose)
     out += _write_output(parameters, numbers, verbose)
-    
-    if numbers["NWNOD"]:
-        out += _write_elements(parameters, verbose)
-
-    if numbers["NWCOM"]:
-        out += _write_indices_names(parameters, "components", numbers["NWCOM"], verbose)
-
-    if numbers["NWMIN"]:
-        out += _write_indices_names(parameters, "minerals", numbers["NWMIN"], verbose)
-
-    if numbers["NWAQ"]:
-        out += _write_indices_names(parameters, "aqueous_species", numbers["NWAQ"], verbose)
-
-    if numbers["NWADS"]:
-        out += _write_indices_names(parameters, "surface_complexes", numbers["NWADS"], verbose)
-
-    if numbers["NWEXC"]:
-        out += _write_indices_names(parameters, "exchange_species", numbers["NWEXC"], verbose)
-
+    out += _write_elements(parameters, numbers["NWNOD"], verbose)
+    out += _write_indices_names(parameters, "components", numbers["NWCOM"], verbose)
+    out += _write_indices_names(parameters, "minerals", numbers["NWMIN"], verbose)
+    out += _write_indices_names(parameters, "aqueous_species", numbers["NWAQ"], verbose)
+    out += _write_indices_names(parameters, "surface_complexes", numbers["NWADS"], verbose)
+    out += _write_indices_names(parameters, "exchange_species", numbers["NWEXC"], verbose)
     out += _write_default(parameters, verbose, mopr_11)
+    out += _write_zones(parameters, verbose, mopr_11)
+    out += _write_convergence_bounds(parameters, verbose, mopr_10)
+    out += ["end\n"]
 
     return out
 
@@ -86,8 +74,7 @@ def _generate_numbers(parameters):
 
 
 def _write_title(parameters, verbose):
-    out = []
-    out += ["# Title\n"] if verbose else []
+    out = ["# Title\n"] if verbose else []
     out += [f"{parameters['title']}\n"]
 
     return out
@@ -140,16 +127,7 @@ def _write_options(parameters, verbose):
 
     if verbose:
         out += ["#   SL1MIN     RCOUR    STIMAX    CNFACT\n"]
-        out += " {}\n".format(
-            " ".join(
-                [
-                    to_str(values[0], "{{:9f}}"),
-                    to_str(values[1], "{{:9f}}"),
-                    to_str(values[2], "{{:9f}}"),
-                    to_str(values[3], "{{:9f}}"),
-                ]
-            )
-        )
+        out += " {}\n".format(" ".join(to_str(value, "{{:9f}}") for value in values))
 
     else:
         out += f"{' '.join(str(x) for x in values)}\n"
@@ -158,7 +136,7 @@ def _write_options(parameters, verbose):
 
 
 def _write_filenames(parameters, verbose):
-    out = []
+    out = ["# Input and output file names\n"] if verbose else []
     values = [
         _get(parameters, ("files", "thermodynamic_input"), ""),
         _get(parameters, ("files", "iteration_output"), ""),
@@ -169,7 +147,6 @@ def _write_filenames(parameters, verbose):
     ]
 
     if verbose:
-        out += ["# Input and output file names\n"]
         out += [f"{values[0]:<20}  ! Thermodynamic database\n"]
         out += [f"{values[1]:<20}  ! Iteration information\n"]
         out += [f"{values[2]:<20}  ! Aqueous concentrations in Tecplot form\n"]
@@ -184,7 +161,7 @@ def _write_filenames(parameters, verbose):
 
 
 def _write_weights_coefficients(parameters, verbose):
-    out = []
+    out = ["#    ITIME      WUPC     DFFUN    DFFUNG\n"] if verbose else []
     values = [
         _get(parameters, ("options", "w_time"), 0.0),
         _get(parameters, ("options", "w_upstream"), 0.0),
@@ -193,17 +170,7 @@ def _write_weights_coefficients(parameters, verbose):
     ]
 
     if verbose:
-        out += ["#    ITIME      WUPC     DFFUN    DFFUNG\n"]
-        out += " {}\n".format(
-            " ".join(
-                [
-                    to_str(values[0], "{{:9f}}"),
-                    to_str(values[1], "{{:9f}}"),
-                    to_str(values[2], "{{:9f}}"),
-                    to_str(values[3], "{{:9f}}"),
-                ]
-            )
-        )
+        out += " {}\n".format(" ".join(to_str(value, "{{:9f}}") for value in values))
 
     else:
         out += f"{' '.join(str(x) for x in values)}\n"
@@ -212,7 +179,7 @@ def _write_weights_coefficients(parameters, verbose):
 
 
 def _write_convergence(parameters, verbose):
-    out = []
+    out = ["# MAXITPTR     TOLTR  MAXITPCH     TOLCH     TOLMB     TOLDC     TOLDR\n"] if verbose else []
     values = [
         _get(parameters, ("options", "n_iteration_tr"), 0),
         _get(parameters, ("options", "eps_tr"), 0.0),
@@ -224,7 +191,6 @@ def _write_convergence(parameters, verbose):
     ]
 
     if verbose:
-        out += ["# MAXITPTR     TOLTR  MAXITPCH     TOLCH     TOLMB     TOLDC     TOLDR\n"]
         out += " {}\n".format(
             " ".join(
                 [
@@ -246,7 +212,7 @@ def _write_convergence(parameters, verbose):
 
 
 def _write_output(parameters, numbers, verbose):
-    out = []
+    out = ["#  NWTI  NWNOD  NWCOM  NWMIN   NWAQ  NWADS  NWEXC   ICON    MIN   IGAS\n"] if verbose else []
     values = [
         _get(parameters, ("options", "n_cycle_print"), 0),
         numbers["NWNOD"],
@@ -261,23 +227,7 @@ def _write_output(parameters, numbers, verbose):
     ]
 
     if verbose:
-        out += ["#  NWTI  NWNOD  NWCOM  NWMIN   NWAQ  NWADS  NWEXC   ICON    MIN   IGAS\n"]
-        out += " {}\n".format(
-            " ".join(
-                [
-                    to_str(values[0], "{:6d}"),
-                    to_str(values[1], "{:6d}"),
-                    to_str(values[2], "{:6d}"),
-                    to_str(values[3], "{:6d}"),
-                    to_str(values[4], "{:6d}"),
-                    to_str(values[5], "{:6d}"),
-                    to_str(values[6], "{:6d}"),
-                    to_str(values[7], "{:6d}"),
-                    to_str(values[8], "{:6d}"),
-                    to_str(values[9], "{:6d}"),
-                ]
-            )
-        )
+        out += " {}\n".format(" ".join(to_str(value, "{:6d}") for value in values))
 
     else:
         out += f"{' '.join(str(x) for x in values)}\n"
@@ -285,9 +235,14 @@ def _write_output(parameters, numbers, verbose):
     return out
 
 
-def _write_elements(parameters, verbose):
-    out = []
-    out += [f"# {comments['nodes']}\n"] if verbose else []
+def _write_elements(parameters, n, verbose):
+    out = [f"# {comments['nodes']}\n"] if verbose else []
+
+    if n == 0:
+        out += ["\n"]
+
+        return out
+    
     out += [f"{x[:5]}\n" for x in parameters["output"]["elements"]]
     out += ["\n"]
 
@@ -295,8 +250,12 @@ def _write_elements(parameters, verbose):
 
 
 def _write_indices_names(parameters, key, n, verbose):
-    out = []
-    out += [f"# {comments[key]}\n"] if verbose else []
+    out = [f"# {comments[key]}\n"] if verbose else []
+
+    if n == 0:
+        out += ["\n"]
+
+        return out
 
     if n > 0:
         out += f"{' '.join(str(x) for x in parameters['output'][key])}\n"
@@ -309,7 +268,7 @@ def _write_indices_names(parameters, key, n, verbose):
 
 
 def _write_default(parameters, verbose, mopr_11=0):
-    out = []
+    out = [f"#IZIWDF IZBWDF IZMIDF IZGSDF IZADDF IZEXDF IZPPDF IZKDDF IZBGDF\n"] if verbose else []
     values = [
         _get(parameters, ("default", "initial_water"), 0),
         _get(parameters, ("default", "injection_water"), 0),
@@ -330,8 +289,92 @@ def _write_default(parameters, verbose, mopr_11=0):
         values += [_get(parameters, ("default", "sedimentation_velocity"), 0)]
 
     if verbose:
-        out += [f"# IZIWDF IZBWDF IZMIDF IZGSDF IZADDF IZEXDF IZPPDF IZKDDF IZBGDF\n"]
-        out += "  {}\n".format(" ".join(to_str(value, "{:6d}") for value in values))
+        out += " {}\n".format(" ".join(to_str(value, "{:6d}") for value in values))
+
+    else:
+        out += f"{' '.join(str(x) for x in values)}\n"
+
+    return out
+
+
+def _write_zones(parameters, verbose, mopr_11=0):
+    out = ["#ELEM NSEQ NADD IZIW IZBW IZMI IZGS IZAD IZEX IZPP IZKD IZBG\n"] if verbose else []
+    
+    if not parameters["zones"]:
+        out += ["\n"]
+
+        return out
+    
+    # Do not use items() for better warning logging in function _get
+    for zone in parameters["zones"]:
+        values = [
+            zone,
+            _get(parameters, ("zones", zone, "nseq"), 0),
+            _get(parameters, ("zones", zone, "nadd"), 0),
+            _get(parameters, ("zones", zone, "initial_water"), 0),
+            _get(parameters, ("zones", zone, "injection_water"), 0),
+            _get(parameters, ("zones", zone, "mineral"), 0),
+            _get(parameters, ("zones", zone, "gas"), 0),
+            _get(parameters, ("zones", zone, "adsorption"), 0),
+            _get(parameters, ("zones", zone, "ion_exchange"), 0),
+            _get(parameters, ("zones", zone, "porosity_permeability"), 0),
+            _get(parameters, ("zones", zone, "linear_adsorption"), 0),
+            _get(parameters, ("zones", zone, "injection_gas"), 0),
+        ]
+
+        if mopr_11 != 1:
+            if "element" in parameters["zones"][zone]:
+                values += [parameters["zones"][zone]["element"]]
+
+        else:
+            values += [_get(parameters, ("zones", zone, "sedimentation_velocity"), 0)]
+
+        if verbose:
+            out += f"{values[0][:5]} {' '.join(to_str(value, '{:4d}') for value in values[1:])}\n"
+
+        else:
+            out += f"{' '.join(str(x) for x in values)}\n"
+
+    out += ["\n"]
+
+    return out
+
+
+def _write_convergence_bounds(parameters, verbose, mopr_10=0):
+    if mopr_10 != 2:
+        return []
+
+    out = ["CONVP\n"]
+    out += ["# Read in chemical convergence limits if MOPR(10)=2\n"] if verbose else []
+
+    # Numbers of iterations
+    values = [
+        _get(parameters, ("options", "n_iteration_1"), 30),
+        _get(parameters, ("options", "n_iteration_2"), 50),
+        _get(parameters, ("options", "n_iteration_3"), 75),
+        _get(parameters, ("options", "n_iteration_4"), 100),
+    ]
+
+    if verbose:
+        out += ["# MAXCHEM1  MAXCHEM2  MAXCHEM3  MAXCHEM4\n"]
+        out += " {}\n".format(" ".join(to_str(value, "{:9d}") for value in values))
+
+    else:
+        out += f"{' '.join(str(x) for x in values)}\n"
+
+    # Increase/reduce factors
+    values = [
+        _get(parameters, ("options", "t_increase_factor_1"), 2.0),
+        _get(parameters, ("options", "t_increase_factor_2"), 1.5),
+        _get(parameters, ("options", "t_increase_factor_3"), 1.0),
+        _get(parameters, ("options", "t_reduce_factor_1"), 0.8),
+        _get(parameters, ("options", "t_reduce_factor_2"), 0.6),
+        _get(parameters, ("options", "t_reduce_factor_3"), 0.5),
+    ]
+
+    if verbose:
+        out += ["#  DTINCR1   DTINCR2   DTINCR3   DTDECR1   DTDECR2   DTDECR3\n"]
+        out += " {}\n".format(" ".join(to_str(value, "{{:9f}}") for value in values))
 
     else:
         out += f"{' '.join(str(x) for x in values)}\n"
@@ -357,9 +400,9 @@ def _get(parameters, keys, default):
             if isinstance(keys, str)
             else "".join(f"['{key}']" for key in keys)
         )
-        logging.warning(f"Key {key_str} is not defined. Set to {default} by default.")
+        logging.warning(f"Key {key_str} is not defined. Setting to {default}.")
 
-        return 
+        return default
         
 
 comments = {
