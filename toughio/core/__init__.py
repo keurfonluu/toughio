429866677dbe029384744a31d3e87866e3d611bc
Keurfon Luu <keurfonluu@outlook.com>
2025-05-13 17:26:43 +0200

    move well output to well core module

--- toughio/core/__init__.py
+++ toughio/core/__init__.py
@@ -2,9 +2,9 @@ from . import capillarity, relative_permeability
 from .exceptions import ReadError
 from .file import FileIterator
 from .h5file import H5File
-from .history_output import HistoryOutput, WellOutput
+from .history_output import HistoryOutput
 from .labeler import Labeler
 from .mesh import CylindricMesh, Mesh
 from .output import ConnectionOutput, ElementOutput, Output
 from .particle_tracker import ParticleTracker
-from .well import Pipe, WellCasing
+from .well import Pipe, WellCasing, WellOutput
