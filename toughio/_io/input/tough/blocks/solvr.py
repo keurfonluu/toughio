bee50a65d35c4606b19829cec4cb5bc7c7ee3362
Keurfon Luu <keurfonluu@outlook.com>
2025-06-10 11:47:52 +0200

    fix block solvr for tough4

--- toughio/_io/input/tough/blocks/solvr.py
+++ toughio/_io/input/tough/blocks/solvr.py
@@ -7,11 +7,12 @@ from .....core import DataBlock, FileIterator
 
 class SOLVR(DataBlock):
     name = "SOLVR"
-    formats = {1: "1s,4S,5S,10f,10f"}
+    formats = {1: "1s,4S,5S,10f,10f,10d"}
 
     def _read(
         self,
         f: FileIterator | TextIO | str,
+        simulator: str,
         *args,
         **kwargs
     ) -> dict:
@@ -27,11 +28,15 @@ class SOLVR(DataBlock):
             "rel_iter_max": data[3],
             "eps": data[4],
         }
+
+        if simulator == "tough4":
+            solvr["solver"]["n_iteration"] = data[5]
+
         solvr["solver"] = self.prune_values(solvr["solver"])
 
         return solvr
 
-    def _write(self, parameters: dict, *args, **kwargs) -> list[str]:
+    def _write(self, parameters: dict, simulator: str, *args, **kwargs) -> list[str]:
         """Write SOLVR block data."""
         values = [
             str(parameters["solver"].get("method", "")),
@@ -40,6 +45,10 @@ class SOLVR(DataBlock):
             parameters["solver"].get("rel_iter_max"),
             parameters["solver"].get("eps"),
         ]
+
+        if simulator == "tough4":
+            values.append(parameters["solver"].get("n_iteration"))
+
         out = self.writers[1](values)
 
         return out
