e62b6c0772f5cbe9a1827a13685912e526790555
Keurfon Luu <keurfonluu@outlook.com>
2025-08-14 23:30:53 +0700

    give priority to tough default format (last position)

--- toughio/_io/input/tough/__init__.py
+++ toughio/_io/input/tough/__init__.py
@@ -2,5 +2,5 @@ from ._read import read
 from ._write import write
 from .._helpers import register
 
-for file_format in ("tough", "tough3", "tough4"):
+for file_format in ("tough4", "tough3", "tough"):
     register(file_format, [""], read, write)
