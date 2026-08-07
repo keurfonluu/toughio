178f426250b95ddf0fc34a7ac49cf2058fe1d969
Keurfon Luu <keurfonluu@outlook.com>
2026-08-07 17:04:32 +0200

    small optimization

--- toughio/utils/_helpers.py
+++ toughio/utils/_helpers.py
@@ -212,16 +212,23 @@ def load_element_history(
 
     for filename in filenames:
         with H5File(filename) as f:
+            foft_list = set(f.list_element_history())
+
             for k, v in names_.items():
+                v = v.replace(" ", "_")
+
+                if v not in foft_list:
+                    raise ValueError(f"could not find element history for label '{v}' in '{filename}'")
+
                 foft_ = foft.setdefault(k, HistoryOutput())
-                foft_ += f.load_element_history(v.replace(" ", "_"))
+                foft_ += f.load_element_history(v)
 
     return foft
 
 
 def load_rock_history(
     filename: str | os.PathLike | Sequence[str | os.PathLike],
-    interfaces: Sequence[tuple[str, str]] | dict[str, Sequence[tuple[str, str]]],
+    names: Sequence[tuple[str, str]] | dict[str, Sequence[tuple[str, str]]],
 ) -> dict[str, RockHistoryOutput]:
     """
     Load and aggregate rock history outputs from one or more H5 files.
@@ -231,7 +238,7 @@ def load_rock_history(
     filename : str | PathLike | Sequence[str | PathLike]
         H5 filename(s) to load rock history outputs from. Files must be sorted in
         chronological order.
-    interfaces : Sequence[tuple[str, str]] | dict[str, Sequence[tuple[str, str]]]
+    names : Sequence[tuple[str, str]] | dict[str, Sequence[tuple[str, str]]]
         Interfaces to load rock history outputs for.
 
     Returns
@@ -244,44 +251,39 @@ def load_rock_history(
 
     # Normalize inputs
     filenames = [filename] if isinstance(filename, (str, os.PathLike)) else filename
-
-    if not isinstance(interfaces, dict):
-        interfaces_ = {"-".join(interface): [interface] for interface in interfaces}
-
-    else:
-        interfaces_ = {k: list(v) for k, v in interfaces.items()}
+    names_ = (
+        {"-".join(name): [name] for name in names}
+        if not isinstance(names, dict)
+        else {k: v for k, v in names.items()}
+    )
 
     # Loop over files and interfaces
     roft = {}
 
     for filename in filenames:
         with H5File(filename) as f:
-            roft_list = f.list_rock_history()
+            roft_list = set(f.list_rock_history())
 
-            for k, v in interfaces_.items():
+            for k, v in names_.items():
                 for connection in v:
                     name = "-".join(connection)
 
-                    if name in roft_list:
-                        roft_ = roft.setdefault(k, {}).setdefault(name, RockHistoryOutput())
-                        roft_ += f.load_rock_history(name)
-
-                    else:
+                    if name not in roft_list:
                         raise ValueError(f"could not find rock history for interface '{name}' in '{filename}'")
 
-    # Aggregate data for each interface
-    result = {}
+                    roft_ = roft.setdefault(k, {}).setdefault(name, RockHistoryOutput())
+                    roft_ += f.load_rock_history(name)
 
-    for k, v in roft.items():
-        first = next(iter(v.values()))
-        data = {
-            "TIME": first.data["TIME"],
-            **{
-                k: np.sum([vv.data[k] for vv in v.values()], axis=0)
-                for k in first.data
-                if k != "TIME"
+    return {
+        k: RockHistoryOutput(
+            {
+                "TIME": next(iter(v.values())).data["TIME"],
+                **{
+                    k: np.sum([vv.data[k] for vv in v.values()], axis=0)
+                    for k in next(iter(v.values())).data
+                    if k != "TIME"
+                }
             }
-        }
-        result[k] = RockHistoryOutput(data)
-
-    return result
+        )
+        for k, v in roft.items()
+    }
